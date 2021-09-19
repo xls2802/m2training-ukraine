d281ed617eb85cdd6ac5d33b41d098a1d5dc0ac9
SerjG <xls2802@gmail.com>
2021-09-19 23:10:10 +0300

    1.3. di.xml part 2

---
+++ app/code/Training/AdditionalTestOM/Model/ManagerCustom.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Training\AdditionalTestOM\Model;
+
+use Training\TestOM\Model\ManagerInterface;
+
+/**
+ * Class ManagerCustom
+ * @package Training\AdditionalTestOM\Model
+ */
+class ManagerCustom implements ManagerInterface
+{
+    /**
+     * @return void
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function get()
+    {
+    }
+}
