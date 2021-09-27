10ce687894669ed5e183cc4adafd657d1f7094b9
SerjG <xls2802@gmail.com>
2021-09-27 16:14:38 +0300

    2.3. Routing NoRouteHandler add annotations

--- app/code/Training/Test/App/Router/NoRouteHandler.php
+++ app/code/Training/Test/App/Router/NoRouteHandler.php
@@ -2,8 +2,16 @@
 
 namespace Training\Test\App\Router;
 
+/**
+ * Class NoRouteHandler
+ * @package Training\Test\App\Router
+ */
 class NoRouteHandler implements \Magento\Framework\App\Router\NoRouteHandlerInterface
 {
+    /**
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @return bool
+     */
     public function process(\Magento\Framework\App\RequestInterface $request)
     {
         $moduleName = 'cms';
