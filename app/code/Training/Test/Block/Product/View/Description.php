2cf64b38d44d35e2565740b8c6840846372f958a
SerjG <xls2802@gmail.com>
2021-10-06 00:16:58 +0300

    3.7. Rendering. Custom description template

--- app/code/Training/Test/Block/Product/View/Description.php
+++ app/code/Training/Test/Block/Product/View/Description.php
@@ -6,6 +6,6 @@ class Description
     public function beforeToHtml(
         \Magento\Catalog\Block\Product\View\Description $subject
     ) {
-        $subject->getProduct()->setDescription('Test description');
+        $subject->setTemplate('Training_Test::description.phtml');
     }
 }
