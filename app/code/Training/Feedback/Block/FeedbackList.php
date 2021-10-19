3967ce0278fd813923c4c5336493c04dea5a4dd6
SerjG <xls2802@gmail.com>
2021-10-20 00:44:47 +0300

    5.1. Models. Module Training_Feedback

---
+++ app/code/Training/Feedback/Block/FeedbackList.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Training\Feedback\Block;
+
+use Magento\Framework\DataObject;
+use Magento\Framework\Stdlib\DateTime\Timezone;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Theme\Block\Html\Pager;
+use Training\Feedback\Model\ResourceModel\Feedback\CollectionFactory;
+
+class FeedbackList extends Template
+{
+    const PAGE_SIZE = 5;
+    private $collectionFactory;
+    private $collection;
+    private $timezone;
+
+    /**
+     * FeedbackList constructor.
+     * @param Context $context
+     * @param CollectionFactory $collectionFactory
+     * @param Timezone $timezone
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        CollectionFactory $collectionFactory,
+        Timezone $timezone,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->collectionFactory = $collectionFactory;
+        $this->timezone = $timezone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCollection()
+    {
+        if (!$this->collection) {
+            $this->collection = $this->collectionFactory->create();
+            $this->collection->addFieldToFilter('is_active', 1);
+            $this->collection->setOrder('creation_time', 'DESC');
+        }
+        return $this->collection;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPagerHtml()
+    {
+        $pagerBlock = $this->getChildBlock('feedback_list_pager');
+        if ($pagerBlock instanceof DataObject) {
+            /* @var $pagerBlock Pager */
+            $pagerBlock
+                ->setUseContainer(false)
+                ->setShowPerPage(false)
+                ->setShowAmounts(false)
+                ->setLimit($this->getLimit())
+                ->setCollection($this->getCollection());
+            return $pagerBlock->toHtml();
+        }
+        return '';
+    }
+
+    /**
+     * @return int
+     */
+    public function getLimit()
+    {
+        return static::PAGE_SIZE;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddFeedbackUrl()
+    {
+        return $this->getUrl('training_feedback/index/form');
+    }
+
+    /**
+     * @param $feedback
+     * @return false|string
+     */
+    public function getFeedbackDate($feedback)
+    {
+        return $this->timezone->formatDateTime($feedback->getCreationTime());
+    }
+}
